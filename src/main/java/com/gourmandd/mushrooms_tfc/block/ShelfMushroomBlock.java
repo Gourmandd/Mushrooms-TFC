92bbcffdb479d579c085099f2146e5c2e6144a70
Gourmandd <149611363+Gourmandd@users.noreply.github.com>
2026-05-25 19:45:05 +0100

    unused import.

--- src/main/java/com/gourmandd/mushrooms_tfc/block/ShelfMushroomBlock.java
+++ src/main/java/com/gourmandd/mushrooms_tfc/block/ShelfMushroomBlock.java
@@ -25,7 +25,6 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
-import javax.annotation.Nullable;
 import java.util.Map;
 import java.util.function.Supplier;
 
